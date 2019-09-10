89daedea658916d83b19484af9da377f2086b2fb
Dj-Polyester <anonymous_571@outlook.com>
2019-09-10 12:08:31 +0300

    Just changed a line

--- bash_automation/cpp.sh
+++ bash_automation/cpp.sh
@@ -30,7 +30,7 @@ else
   		echo "#include <iostream>" >> ${filename}.cpp
   		
   		printf "\nusing namespace std;\n\nint main()\n{\n\n	return 0;\n}" >> ${filename}.cpp
-  		gedit ${filename}.cpp &
+  		${program} ${filename}.cpp &
 	done
 		
 fi
