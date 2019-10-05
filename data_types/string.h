a3392b565580908bebc9f05dfc9cc562dc7cb656
Dj-Polyester <anonymous_571@outlook.com>
2019-10-05 16:23:29 +0300

    Fixes on string

--- data_types/string.h
+++ data_types/string.h
@@ -10,12 +10,13 @@ private:
     char* str_data;
 public:
     String(const char* str=NULL);
+    String(const String& other);
     ~String();
-    void print();
-    void operator = (const char*& str);
-    String operator + (const String& str);
+    String& operator = (const char* &str);
+    String& operator = (const String &str);
     //friend method, to access private variables of std class
     friend std::ostream& operator<<(std::ostream& out, const String &str);
+    void print();
     char* data() const {return str_data;}
     void set_data(const char* str);
 };
